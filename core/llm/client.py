90900f51a91ad6ef6f5c36da1c8c2d9cff313b1e
s.nikulin <s.nikulin@rogii.com>
2025-05-31 20:35:47 +0300

    Add LLM Client

---
+++ core/llm/client.py
@@ -0,0 +1,11 @@
+from openai import OpenAI
+from core.config.settings import settings
+from telethon.tl.types import Message
+
+
+class LLMClient:
+    def __init__(self, api_key: str):
+        self.client = OpenAI(api_key=settings.LLM_API, base_url=settings.LLM_URL)
+
+    def generate_summary(self, messages: list[Message]) -> str:
+        pass
